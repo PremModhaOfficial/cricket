002b5d7b6cf333d4cc12bfdad8a64b6da330a244
Prem <premmodhaofficial@gmail.com>
2024-04-06 10:56:04 +0530

    Done it works

--- main.py
+++ main.py
@@ -7,36 +7,35 @@ NOBALL = {"RUN": 1, "RETHROW": True}
 WIDEBALL = {"RUN": 1, "RETHROW": True}
 PLAYERS_PER_TEAM = 11
 
-
 scorer = Scorer()
 
 score_writer = ScoreWriter(scorer)
-#scorer.match_reader(score_parcer.score)
+scorer.match_reader(score_parcer.score)
 
-scorer.match_reader(score_writer.get_team_names())
-scorer.match_reader(score_writer.get_toss_info())
-
-scorer.match_reader(score_writer.get_init_batsman())
-
-# play overs
-total_overs = int(input('How many overs would you like to play? '))
-for over in range(total_overs):
-    print()
-    scorer.match_reader(score_writer.get_bowler_name())
-    print("over", over + 1, score_writer.scorer.current_baller.name)
-    scorer.match_reader(score_writer.get_over())
-
-scorer.match_reader(score_writer.change_sides())
-scorer.match_reader(score_writer.get_init_batsman())
-for over in range(total_overs):
-    print()
-    scorer.match_reader(score_writer.get_bowler_name())
-    print("over", over + 1, score_writer.scorer.current_baller.name)
-    scorer.match_reader(score_writer.get_over())
-# scorer.match_reader(score)
-
-plot = Plotter(scorer)
-plot.plot_over_wise_runs()
-print(score_writer.log)
-
-scorer.match_reader(score_writer.end_game())
+# scorer.match_reader(score_writer.get_team_names())
+# scorer.match_reader(score_writer.get_toss_info())
+#
+# scorer.match_reader(score_writer.get_init_batsman())
+#
+# # play overs
+# total_overs = int(input('How many overs would you like to play? '))
+# for over in range(total_overs):
+#     print()
+#     scorer.match_reader(score_writer.get_bowler_name())
+#     print("over", over + 1, score_writer.scorer.current_baller.name)
+#     scorer.match_reader(score_writer.get_over())
+#
+# scorer.match_reader(score_writer.change_sides())
+# scorer.match_reader(score_writer.get_init_batsman())
+# for over in range(total_overs):
+#     print()
+#     scorer.match_reader(score_writer.get_bowler_name())
+#     print("over", over + 1, score_writer.scorer.current_baller.name)
+#     scorer.match_reader(score_writer.get_over())
+# # scorer.match_reader(score)
+#
+# plot = Plotter(scorer)
+# plot.plot_over_wise_runs()
+# print(score_writer.log)
+#
+# scorer.match_reader(score_writer.end_game())
